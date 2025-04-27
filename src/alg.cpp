25c29e3416c2859650a96ad7d26470c83c6993cc
squirrelAf <aleckraowo@gmail.com>
2025-04-27 23:49:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
- int count = 0;
+int countPairs1(int* arr, int len, int value) {
+int count = 0;
   for (int a = 0; a < len; a++) {
     for (int b = a + 1; b < len; b++) {
       if (arr[a] + arr[b] == value) {
@@ -12,8 +12,8 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
-   int left = 0;
+int countPairs2(int* arr, int len, int value) {
+  int left = 0;
   int right = len - 1;
   int cnt = 0;
   while (left < right) {
@@ -34,7 +34,7 @@ int countPairs2(int *arr, int len, int value) {
   }
   return cnt;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; ++i) {
     int pos = value - arr[i];
@@ -52,7 +52,7 @@ int countPairs3(int *arr, int len, int value) {
     right = len - 1;
     while (left <= right) {
       int mi = (left + right) / 2;
-      if (arr[mi] <= target) {
+      if (arr[mi] <= pos) {
         left = mi + 1;
       } else {
         right = mi - 1;
