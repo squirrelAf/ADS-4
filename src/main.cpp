57dd55f3e9a56228a160fad7b9043fdf39ee7e44
squirrelAf <aleckraowo@gmail.com>
2025-06-05 17:44:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,63 @@
 // Copyright 2021 NNTU-CS
 
-int main() {
-    return 0;
+int countPairs1(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+int countPairs2(int *arr, int len, int value) {
+  int count = 0;
+  int end = len - 1;
+  while (end > 0) {
+    if (arr[end] > value) {
+      end--;
+    } else {
+      break;
+    }
+  }
+  for (int i = 0; i < len; i++) {
+    for (int j = end; j > i; j--) {
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+  return count;
+}
+int countPairs3(int *arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int targ = value - arr[i];
+    int low = i + 1;
+    int high = len - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] >= targ) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    int first = low;
+    low = i + 1;
+    high = len - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] <= targ) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    int last = high;
+    if (first <= last) {
+      count += last - first + 1;
+    }
+  }
+  return count;
 }
