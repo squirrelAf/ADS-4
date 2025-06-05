09e1361515256fdf92d11f0a0c634ddc53c8a9d2
squirrelAf <aleckraowo@gmail.com>
2025-06-05 18:31:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-countPairs1(int *arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
@@ -12,7 +12,7 @@ countPairs1(int *arr, int len, int value) {
   return count;
 }
 
-countPairs2(int *arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int end = len - 1;
   while (end > 0) {
@@ -31,7 +31,7 @@ countPairs2(int *arr, int len, int value) {
   return count;
 }
 
-countPairs3(int *arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     int targ = value - arr[i];
